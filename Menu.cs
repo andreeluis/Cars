7c5ab9fcab325d4a887ee2cc601d135f93a14244
André Luís <andreluis2442@gmail.com>
2022-08-24 20:21:06 -0300

    Creating ViewCar method

--- Menu.cs
+++ Menu.cs
@@ -4,24 +4,26 @@
     {
         public static void Show()
         {
+            Console.Clear();
             Console.WriteLine("Gerenciador de Carros - POO");
 
             Console.WriteLine("01 - Adicionar novo carro\n" +
                               "02 - Ver carros registrados\n" +
                               "03 - Editar carro existente\n" +
-                              "04 - Remover carro existente\n\n" +
-                              "00 - Sair");
-            Console.WriteLine("Digite a opção que deseja:");
+                              "04 - Remover carro existente\n" +
+                              "00 - Sair\n");
+            Console.Write("Digite a opção que deseja: ");
             switch (Convert.ToInt16(Console.ReadLine()))
             {
                 case 1: AddCar(); break;
-                case 2:
+                case 2: ViewCars(); break;
                 case 3:
                 case 4:
                 case 0: Console.Clear(); Environment.Exit(0); break;
                 default: Console.Clear(); Show(); break;
             }
         }
+
         public static void AddCar()
         {
             Console.WriteLine("Digite as informações sobre o carro:\n");
@@ -50,15 +52,31 @@
 
             Data.Create(new Car(brand, model, color, km));
 
-            Thread.Sleep(1000);
-            Console.Write("\nAdicionando carro aos registros.");
-            Thread.Sleep(800);
+            Console.Write("\nAdicionando carro aos registros .");
+            Thread.Sleep(300);
             Console.Write(" .");
-            Thread.Sleep(800);
+            Thread.Sleep(300);
             Console.Write(" .");
-            Thread.Sleep(800);
-            Console.WriteLine("Carro adicionado com sucesso.");
+            Thread.Sleep(300);
+            Console.Write("\n\nCarro adicionado com sucesso.");
+            Thread.Sleep(2500);
+            Menu.Show();
+        }
 
+        private static void ViewCars()
+        {
+            Console.WriteLine("Estes são os carros cadastrados:");
+            Console.WriteLine("*----*--***--*----*");
+            foreach (Car car in Data.Read())
+            {
+                Console.WriteLine($"Carro {car.Id}:");
+                Console.WriteLine($"\nMarca: {car.Brand}");
+                Console.WriteLine($"Modelo: {car.Model}");
+                Console.WriteLine($"Cor: {car.Color}");
+                Console.WriteLine($"Quilometragem: {car.Km}");
+                Console.WriteLine("\n---*--*--*---");
+            }
+            
         }
     }
 }
